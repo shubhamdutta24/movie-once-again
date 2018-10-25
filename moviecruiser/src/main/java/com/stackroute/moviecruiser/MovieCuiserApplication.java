f4a1363a3f1ba8ae78b90ad5e12a326458cd8a2e
Shubham Kumar Dutta <shubhamkumardutta24@gmail.com>
2018-10-25 17:05:23 +0530

    initialisation

---
+++ moviecruiser/src/main/java/com/stackroute/moviecruiser/MovieCuiserApplication.java
@@ -0,0 +1,12 @@
+package com.stackroute.moviecruiser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieCuiserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieCuiserApplication.class, args);
+	}
+}
